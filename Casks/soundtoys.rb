48fb1ac4cc95e11b561e2bbc6d0a116e2b423971
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2021-03-06 14:36:08 +1100

    Add cask for soundtoys.

---
+++ Casks/soundtoys.rb
@@ -0,0 +1,9 @@
+cask "soundtoys" do
+  version "5.3.3.15699"
+  sha256 "4be37b610bd19bde6511ab5901b3b46be89d4665c0af7c703c9e22992cfe46ec"
+
+  url "https://storage.googleapis.com/soundtoys-download/versions/version_5_3_3_15699/Soundtoys5_5.3.3.15699.dmg"
+  name "Soundtoys 5"
+  desc "Audio plugin fx bundle."
+  homepage "https://www.soundtoys.com/"
+end
